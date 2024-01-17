37316139dbce811c2906f727584e95a868d5404b
seifeddinesaad <saadseif1925@outlook.com>
2024-01-17 19:12:23 +0000

    feat(auth): setting token in local storage and redirect to home pae while login with auth providers

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -11,7 +11,7 @@ import { useFormik } from "formik";
 import * as yup from "yup";
 import "./signIn.css"
 import Link from "next/link";
-import {useRouter} from "next/navigation";
+import { useRouter } from "next/navigation";
 import GuestGuard from "@/components/GuestGuard/GuestGuard";
 
 
@@ -21,7 +21,7 @@ const validationSchema = yup.object({
 });
 
 export default function SignIn() {
-  const router=useRouter();
+  const router = useRouter();
   const formik = useFormik({
     initialValues: {
       email: "",
@@ -30,11 +30,11 @@ export default function SignIn() {
     validationSchema: validationSchema,
     onSubmit: async (values) => {
       try {
-        const userCredential:any = await createUserWithEmailAndPassword(auth, values.email, values.password);
+        const userCredential: any = await createUserWithEmailAndPassword(auth, values.email, values.password);
         // Access the user information from the UserCredential object
         const user = userCredential.user;
         localStorage.setItem('userToken', JSON.stringify(user?.accessToken));
-        router.push("/home");
+        router.push("/");
       } catch (err) {
         console.error(err);
       }
@@ -42,170 +42,175 @@ export default function SignIn() {
   });
   const signInWithGoogle = async () => {
     try {
-      await signInWithPopup(auth, googleProvider);
+      const userCredential = await signInWithPopup(auth, googleProvider);
+      const user: any = userCredential.user;
+      localStorage.setItem('userToken', JSON.stringify(user?.accessToken));
+      router.push("/");
     } catch (err) {
       console.error(err);
     }
   };
   const signInWithGitHub = async () => {
     try {
-      const result = await signInWithPopup(auth, githubProvider);
-      const user = result.user;
+      const userCredential = await signInWithPopup(auth, githubProvider);
+      const user: any = userCredential.user;
+      localStorage.setItem('userToken', JSON.stringify(user?.accessToken));
+      router.push("/");
       console.log("GitHub user:", user);
     } catch (err) {
       console.error(err);
     }
   };
-  
+
 
   return (
     <GuestGuard>
-    <div className="min-h-screen flex flex-col sm:flex-row">
-      <div className="flex-1 py-16 flex flex-col justify-center px-4 sm:px-6  lg:px-20 xl:px-24">
-        <div className="mx-auto w-full max-w-sm lg:max-w-md xl:max-w-lg">
-          <div className="mx-auto form__header">
-            <Image alt="Golobe" className="h-12 w-44" src={Logo} width={100} />
-            <h2 className="mt-6 text-3xl font-extrabold text-gray-900">Login</h2>
-            <p className="mt-2 text-sm text-gray-600">
-              Login to access your Golobe account
-            </p>
-          </div>
+      <div className="min-h-screen flex flex-col sm:flex-row">
+        <div className="flex-1 py-16 flex flex-col justify-center px-4 sm:px-6  lg:px-20 xl:px-24">
+          <div className="mx-auto w-full max-w-sm lg:max-w-md xl:max-w-lg">
+            <div className="mx-auto form__header">
+              <Image alt="Golobe" className="h-12 w-44" src={Logo} width={100} />
+              <h2 className="mt-6 text-3xl font-extrabold text-gray-900">Login</h2>
+              <p className="mt-2 text-sm text-gray-600">
+                Login to access your Golobe account
+              </p>
+            </div>
 
-          <div className="mt-8">
-            <form onSubmit={formik.handleSubmit} className="space-y-6">
-              <div>
-                <label
-                  className="block text-sm font-medium text-gray-700"
-                  htmlFor="email"
-                >
-                  Email
-                </label>
-                <div className="mt-1">
-                  <input
-                    autoComplete="email"
-                    className={`${formik.touched.email && formik.errors.email
-                      ? "border-red-300"
-                      : "border-gray-300"
-                      } appearance-none block w-full px-3 py-2 border rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm`}
-                    id="email"
-                    name="email"
-                    placeholder="john.doe@gmail.com"
-                    type="email"
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                    value={formik.values.email}
-                  />
-                  {formik.touched.email && formik.errors.email && (
-                    <p className="mt-1 text-sm text-red-500">{formik.errors.email}</p>
-                  )}
+            <div className="mt-8">
+              <form onSubmit={formik.handleSubmit} className="space-y-6">
+                <div>
+                  <label
+                    className="block text-sm font-medium text-gray-700"
+                    htmlFor="email"
+                  >
+                    Email
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      autoComplete="email"
+                      className={`${formik.touched.email && formik.errors.email
+                        ? "border-red-300"
+                        : "border-gray-300"
+                        } appearance-none block w-full px-3 py-2 border rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm`}
+                      id="email"
+                      name="email"
+                      placeholder="john.doe@gmail.com"
+                      type="email"
+                      onChange={formik.handleChange}
+                      onBlur={formik.handleBlur}
+                      value={formik.values.email}
+                    />
+                    {formik.touched.email && formik.errors.email && (
+                      <p className="mt-1 text-sm text-red-500">{formik.errors.email}</p>
+                    )}
+                  </div>
                 </div>
-              </div>
-              <div className="space-y-1">
-                <label
-                  className="block text-sm font-medium text-gray-700"
-                  htmlFor="password"
-                >
-                  Password
-                </label>
-                <div className="mt-1">
-                  <input
-                    autoComplete="current-password"
-                    className={`${formik.touched.password && formik.errors.password
-                      ? "border-red-300"
-                      : "border-gray-300"
-                      } appearance-none block w-full px-3 py-2 border rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm`}
-                    id="password"
-                    name="password"
-                    placeholder="******************"
-                    type="password"
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                    value={formik.values.password}
-                  />
-                  {formik.touched.password && formik.errors.password && (
-                    <p className="mt-1 text-sm text-red-500">{formik.errors.password}</p>
-                  )}
+                <div className="space-y-1">
+                  <label
+                    className="block text-sm font-medium text-gray-700"
+                    htmlFor="password"
+                  >
+                    Password
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      autoComplete="current-password"
+                      className={`${formik.touched.password && formik.errors.password
+                        ? "border-red-300"
+                        : "border-gray-300"
+                        } appearance-none block w-full px-3 py-2 border rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm`}
+                      id="password"
+                      name="password"
+                      placeholder="******************"
+                      type="password"
+                      onChange={formik.handleChange}
+                      onBlur={formik.handleBlur}
+                      value={formik.values.password}
+                    />
+                    {formik.touched.password && formik.errors.password && (
+                      <p className="mt-1 text-sm text-red-500">{formik.errors.password}</p>
+                    )}
+                  </div>
                 </div>
-              </div>
-              <div>
-                <button
-                  type="submit"
-                  style={{
-                    backgroundColor: "#8cd4b9ff",
-                  }}
-                  className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                >
-                  Login
-                </button>
-              </div>
-            </form>
-            <div className="mt-6">
-              <div className="relative">
-                <div className="absolute inset-0 flex items-center">
-                  <div className="w-full border-t border-gray-300" />
+                <div>
+                  <button
+                    type="submit"
+                    style={{
+                      backgroundColor: "#8cd4b9ff",
+                    }}
+                    className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  >
+                    Login
+                  </button>
                 </div>
-                <div
-                  className="relative flex justify-center text-sm pb-4"
-                >
-                  <p>
-                    Don’t have an account?
-                    <Link
-                      href="/sign-up"
+              </form>
+              <div className="mt-6">
+                <div className="relative">
+                  <div className="absolute inset-0 flex items-center">
+                    <div className="w-full border-t border-gray-300" />
+                  </div>
+                  <div
+                    className="relative flex justify-center text-sm pb-4"
+                  >
+                    <p>
+                      Don’t have an account?
+                      <Link
+                        href="/sign-up"
+                        style={{
+                          color: "#ff8682ff",
+                          fontSize: "1rem",
+                          cursor: "pointer",
+                          paddingLeft: "0.5rem",
+                          fontWeight: '500'
+                        }}
+                      >
+                        Sign up
+                      </Link>
+                    </p>
+                  </div>
+                  <div className="relative flex justify-center text-sm">
+                    <div className="relative flex justify-center text-sm">
+                      <span className="px-2 bg-white text-gray-500">Or Sign up with</span>
+                    </div>
+                  </div>
+                </div>
+                <div className="mt-6 flex flex-wrap  justify-center gap-3 w-full">
+                  <button onClick={signInWithGitHub} className="w-1/4">
+                    <div
+                      className="w-full inline-flex justify-center py-2 px-4 border border-green-300 rounded-md shadow-sm bg-white text-sm font-medium  hover:bg-gray-50"
                       style={{
-                        color: "#ff8682ff",
-                        fontSize: "1rem",
-                        cursor: "pointer",
-                        paddingLeft: "0.5rem",
-                        fontWeight: '500'
+                        padding: "1rem",
                       }}
                     >
-                      Sign up
-                    </Link>
-                  </p>
+                      <span className="sr-only">Sign in with Github</span>
+                      <FaGithub fontSize="1.2rem" />
+                    </div>
+                  </button>
+                  <button type="submit" className="w-1/4" onClick={signInWithGoogle}>
+                    <div
+                      className="w-full inline-flex justify-center py-2 px-4 border border-green-300 rounded-md shadow-sm bg-white text-sm font-medium  hover:bg-gray-50"
+                      style={{
+                        padding: "1rem",
+                      }}
+                    >
+                      <span className="sr-only">Sign in with Google</span>
+                      <FaGoogle fontSize="1.2rem" />
+                    </div>
+                  </button>
                 </div>
-                <div className="relative flex justify-center text-sm">
-                  <div className="relative flex justify-center text-sm">
-                    <span className="px-2 bg-white text-gray-500">Or Sign up with</span>
-                  </div>
-                </div>
-              </div>
-              <div className="mt-6 flex flex-wrap  justify-center gap-3 w-full">
-                <button onClick={signInWithGitHub} className="w-1/4">
-                  <div
-                    className="w-full inline-flex justify-center py-2 px-4 border border-green-300 rounded-md shadow-sm bg-white text-sm font-medium  hover:bg-gray-50"
-                    style={{
-                      padding: "1rem",
-                    }}
-                  >
-                    <span className="sr-only">Sign in with Github</span>
-                    <FaGithub fontSize="1.2rem"/>
-                  </div>
-                </button>
-                <button type="submit" className="w-1/4" onClick={signInWithGoogle}>
-                  <div
-                    className="w-full inline-flex justify-center py-2 px-4 border border-green-300 rounded-md shadow-sm bg-white text-sm font-medium  hover:bg-gray-50"
-                    style={{
-                      padding: "1rem",
-                    }}
-                  >
-                    <span className="sr-only">Sign in with Google</span>
-                    <FaGoogle fontSize="1.2rem"/>
-                  </div>
-                </button>
               </div>
             </div>
           </div>
         </div>
+        <div className="flex-1 flex justify-center py-16 image">
+          <Image
+            alt="Hotel resort"
+            className="object-cover object-center rounded-lg"
+            height="600"
+            src={SignInImage}
+          />
+        </div>
       </div>
-      <div className="flex-1 flex justify-center py-16 image">
-        <Image
-          alt="Hotel resort"
-          className="object-cover object-center rounded-lg"
-          height="600"
-          src={SignInImage}
-        />
-      </div>
-    </div>
     </GuestGuard>
   );
 }
