289a2999d09d67b9d3d1dc05cae1fd99c437f304
seifeddinesaad <saadseif1925@outlook.com>
2024-02-05 09:26:30 +0000

    feat(find-flight): implementation of fall into travel section

--- src/app/find-flight/page.tsx
+++ src/app/find-flight/page.tsx
@@ -10,7 +10,12 @@ import Header from '@/components/Header/Header';
 import Tabs from '../../components/Tabs';
 import Logo from "../../../public/Logo.png";
 import Card from './components/Card';
-import im1 from "../../../public/FindFlights/im1.png"
+import { Button } from "@/components/ui/button"
+import Image from 'next/image';
+import image1 from "../../../public/FindFlights/im5.png"
+import image2 from "../../../public/FindFlights/im6.png"
+import image3 from "../../../public/FindFlights/im7.png"
+import image4 from "../../../public/FindFlights/im8.png"
 const page = () => {
   const tabs = [
     { id: 1, label: 'Flights', content: <FlightTab />, icon: flightIcon },
@@ -41,6 +46,13 @@ const page = () => {
       imageUrl: "https://i.ibb.co/TmWKLr4/Rectangle-6.png",
       price: "700"
     },
+  ];
+
+  const images = [
+    image1,
+    image2,
+    image3,
+    image4
   ]
   return (
     <div className="flex justify-center items-center flex-col bg-gray-100 w-full overflow-hidden">
@@ -77,16 +89,49 @@ const page = () => {
             />
           })}
         </div>
-        {/* <div className="flex justify-center items-center gap-4 w-full m-10 flex-col sm:flex-row">
-                    {tripsDisplay?.map((trip, index) => {
-                        return <TripDisplay
-                            key={index}
-                            image={trip.image}
-                            title={trip.title}
-                            description={trip.description}
-                        />
-                    })}
-                </div> */}
+      </div>
+      <div className="flex justify-center items-center flex-col gap-10 mt-10 w-[85%]">
+        <div className="self-center sm:self-start flex flex-col gap-1">
+          <h1 className="text-3xl font-bold text-center sm:text-left">Fall into travel</h1>
+          <p className="text-lg text-gray-500 text-center sm:text-left">
+            Going somewhere to celebrate this season? Whether you’re going home or somewhere to roam, we’ve got <br />
+            the travel tools to get you to your destination.</p>
+        </div>
+        <div className="lg:flex lg:items-center lg:justify-between lg:p-8 gap-4">
+          <div className="bg-[#8ed4bbff] lg:flex lg:flex-col lg:justify-between lg:w-1/2  gap-8 border-solid-2 rounded-2xl  p-4">
+            <div>
+              <div className="mt-6 flex items-center justify-between lg:mt-0">
+                <h1 className="text-3xl font-semibold  lg:text-5xl">Backpacking Sri Lanka</h1>
+                <div className="text-right bg-white p-2 border-solid-2 rounded-lg">
+                  <span className="block text-sm font-medium ">From</span>
+                  <span className="block text-2xl font-bold ">$700</span>
+                </div>
+              </div>
+              <p className="mt-4 ">
+                Traveling is a unique experience as it's the best way to unplug from the pushes and pulls of daily life. It
+                helps us to forget about our problems, frustrations, and fears at home. During our journey, we experience
+                life in different ways. We explore new places, cultures, cuisines, traditions, and ways of living.
+              </p>
+            </div>
+            <Button className="bg-white text-black">Book Flight</Button>
+          </div>
+          <div className="mt-6 grid grid-cols-1 sm:grid-cols-2 gap-4 lg:mt-0 lg:w-1/2 lg:grid-cols-2">
+            {images.map((image, index) => {
+              return <Image
+                alt="Tea fields in Sri Lanka"
+                className="rounded-lg"
+                height="200"
+                src={image}
+                style={{
+                  aspectRatio: "300/200",
+                  objectFit: "cover",
+                }}
+                width="300"
+              />
+            })}
+
+          </div>
+        </div>
       </div>
 
       {/* <Footer /> */}
