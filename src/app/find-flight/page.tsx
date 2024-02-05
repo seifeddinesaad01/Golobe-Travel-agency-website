1865cf76243341d16e1b55cf1d074cd717c75cde
seifeddinesaad <saadseif1925@outlook.com>
2024-02-05 10:07:43 +0000

    rafactoring: responsiveness improvements

--- src/app/find-flight/page.tsx
+++ src/app/find-flight/page.tsx
@@ -55,30 +55,30 @@ const page = () => {
           })}
         </div>
       </div>
-      <div className="flex justify-center items-center flex-col gap-10 mt-10 w-[85%]">
+      <div className="flex justify-center items-center flex-col gap-10 mt-10 w-[85%] pb-16 pt-8">
         <div className="self-center sm:self-start flex flex-col gap-1">
           <h1 className="text-3xl font-bold text-center sm:text-left">Fall into travel</h1>
-          <p className="text-lg text-gray-500 text-center sm:text-left">
-            Going somewhere to celebrate this season? Whether you’re going home or somewhere to roam, we’ve got <br />
-            the travel tools to get you to your destination.</p>
+          <p className="text-lg text-black text-center sm:text-left">
+            Going somewhere to celebrate this season? Whether you’re going home or somewhere to roam, we’ve got
+            the travel tools to get you to your <br /> destination.</p>
         </div>
-        <div className="lg:flex lg:items-center lg:justify-between lg:p-8 gap-4">
-          <div className="bg-[#8ed4bbff] lg:flex lg:flex-col lg:justify-between lg:w-1/2  gap-8 border-solid-2 rounded-2xl  p-4">
-            <div>
-              <div className="mt-6 flex items-center justify-between lg:mt-0">
+        <div className="lg:flex lg:items-center lg:justify-between gap-4 flex-wrap lg:flex-nowrap">
+          <div className="bg-[#8ed4bbff] lg:flex lg:flex-col lg:justify-between lg:w-1/2  border-solid-2 rounded-2xl px-8 py-5 h-full">
+            <div className='flex flex-col gap-6 h-full'>
+              <div className="flex items-center justify-between lg:mt-0">
                 <h1 className="text-3xl font-semibold  lg:text-5xl">Backpacking Sri Lanka</h1>
                 <div className="text-right bg-white p-2 border-solid-2 rounded-lg">
                   <span className="block text-sm font-medium ">From</span>
                   <span className="block text-2xl font-bold ">$700</span>
                 </div>
               </div>
-              <p className="mt-4 ">
+              <p>
                 Traveling is a unique experience as it's the best way to unplug from the pushes and pulls of daily life. It
                 helps us to forget about our problems, frustrations, and fears at home. During our journey, we experience
                 life in different ways. We explore new places, cultures, cuisines, traditions, and ways of living.
               </p>
             </div>
-            <Button className="bg-white text-black">Book Flight</Button>
+            <Button className="bg-white text-black text-lg">Book Flight</Button>
           </div>
           <div className="mt-6 grid grid-cols-1 sm:grid-cols-2 gap-4 lg:mt-0 lg:w-1/2 lg:grid-cols-2">
             {images.map((image, index) => {
@@ -86,21 +86,20 @@ const page = () => {
                 key={index}
                 alt="Tea fields in Sri Lanka"
                 className="rounded-lg"
-                height="200"
+                // height="200"
                 src={image}
                 style={{
-                  aspectRatio: "300/200",
+                  //  aspectRatio: "300/200",
                   objectFit: "cover",
+                  width:'100%',
                 }}
                 width="300"
               />
             })}
-
           </div>
         </div>
       </div>
-
-      <Footer /> 
+      <Footer />
     </div>
   )
 }
