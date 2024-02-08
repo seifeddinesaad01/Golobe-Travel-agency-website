40bfe35103e4b92552448fb5ef4ffc04ea0071be
seifeddinesaad <saadseif1925@outlook.com>
2024-02-08 17:22:02 +0000

    feat(ui): adding icons to Dropdown links

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -3,19 +3,26 @@ import { DownOutlined } from '@ant-design/icons';
 import type { MenuProps } from 'antd';
 import { Dropdown, Space } from 'antd';
 import Image from 'next/image';
-
+import profileIcon from "../../public/Home/profile.png";
+import settingsIcon from "../../public/Home/Settings.png";
+import LogoutIcon from "../../public/Home/Logout.png";
+import Link from 'next/link';
+import { LogOutIcon } from 'lucide-react';
 const items: MenuProps['items'] = [
     {
-        label: <a href="https://www.antgroup.com">Profile</a>,
+        label: <Link href="/profile" className='flex items-center justify-start gap-2'><Image src={profileIcon} alt="icon" /> <p>Profile</p></Link>,
         key: '0',
     },
-
+    {
+        label: <Link href="/settings" className='flex items-center justify-start gap-2'><Image src={settingsIcon} alt="icon" /> <p>Settings</p></Link>,
+        key: '1',
+    },
     {
         type: 'divider',
     },
     {
-        label: <a href="https://www.aliyun.com">Logout</a>,
-        key: '1',
+        label: <Link href="/Logout" className='flex items-center justify-start gap-2'><Image src={LogoutIcon} alt="icon" /> <p>Logout</p></Link>,
+        key: '2',
     },
 
 ];
