2ea9e7027bfa53feab19967f6435d9ff25935c29
seifeddinesaad <saadseif1925@outlook.com>
2024-02-14 11:00:25 +0000

    feat:  responsiveness improvements in booking-details page

--- src/app/find-flight/listing/[id]/booking-details/page.tsx
+++ src/app/find-flight/listing/[id]/booking-details/page.tsx
@@ -6,7 +6,6 @@ import stayIcon from "../../../../../../public/Home/bIcon2.png";
 import flightIcon from "../../../../../../public/Home/bIcon1.png";
 import FlighDetailsCard from '@/components/FlightDetailsCard';
 import emirates from "../../../../../../public/FindFlights/EmiratesShow.png"
-import { Divider } from 'antd';
 import Image from 'next/image';
 import Footer from '@/components/Footer';
 import CustomRadio from '@/components/CustomRadio';
@@ -20,7 +19,7 @@ const page = () => {
     return (
         <div className='flex flex-col bg-[#fafafaff]'>
             <Header logo={Logo} bgColor="#fff" color="black" flightIcon={flightIcon} stayIcon={stayIcon} />
-            <div className='flex gap-4 w-[90%] self-center mt-20 mb-20'>
+            <div className='flex flex-col-reverse sm:flex-row gap-4 w-[90%] self-center mt-20 mb-20'>
                 <div className='flex flex-col w-full'>
                     <FlighDetailsCard />
                     <div className='flex flex-col gap-2 bg-[#fff] p-4 rounded-lg shadow-lg self-center w-[95%]'>
@@ -42,57 +41,59 @@ const page = () => {
                         />
                     </div>
                 </div>
-                <div className=" w-1/3 mx-auto bg-white rounded-lg shadow-md overflow-hidden justify-start self-start">
-                    <div className="flex justify-between items-center p-4">
-                        <div className="flex">
-                            <Image
-                                alt="Emirates A380 Airbus"
-                                className="h-24 w-24 rounded-lg"
-                                height="96"
-                                src={emirates}
-                                style={{
-                                    aspectRatio: "96/96",
-                                    objectFit: "cover",
-                                }}
-                                width="96"
-                            />
-                            <div className="ml-4">
-                                <p className="text-sm text-gray-500">Economy</p>
-                                <h3 className="text-lg font-semibold">Emirates A380 Airbus</h3>
-                                <div className="flex items-center mt-1">
-                                    <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-green-600 bg-green-200">
-                                        4.2
-                                    </span>
-                                    <span className="ml-2 text-sm text-gray-600">Very Good 54 reviews</span>
+                <div className="w-full lg:w-1/3 mx-auto bg-white rounded-lg shadow-md overflow-hidden">
+                    <div className="p-4">
+                        <div className="flex justify-between items-center">
+                            <div className="flex">
+                                <Image
+                                    alt="Emirates A380 Airbus"
+                                    className="h-24 w-24 rounded-lg"
+                                    height="96"
+                                    src={emirates}
+                                    style={{
+                                        aspectRatio: "96/96",
+                                        objectFit: "cover",
+                                    }}
+                                    width="96"
+                                />
+                                <div className="ml-4">
+                                    <p className="text-sm text-gray-500">Economy</p>
+                                    <h3 className="text-lg font-semibold">Emirates A380 Airbus</h3>
+                                    <div className="flex items-center mt-1">
+                                        <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-green-600 bg-green-200">
+                                            4.2
+                                        </span>
+                                        <span className="ml-2 text-sm text-gray-600">Very Good 54 reviews</span>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="p-4 border-t border-gray-200">
-                        <h4 className="text-lg font-semibold mb-2">Price Details</h4>
-                        <div className="flex justify-between">
-                            <span>Base Fare</span>
-                            <span>$400</span>
-                        </div>
-                        <div className="flex justify-between">
-                            <span>Discount</span>
-                            <span>$400</span>
-                        </div>
-                        <div className="flex justify-between">
-                            <span>Taxes</span>
-                            <span>$400</span>
-                        </div>
-                        <div className="flex justify-between">
-                            <span>Service Fee</span>
-                            <span>$400</span>
-                        </div>
-                        <Divider />
-                        <div className="flex justify-between font-semibold mt-2">
-                            <span>Total</span>
-                            <span>$400</span>
+                        <div className="p-4 border-t border-gray-200">
+                            <h4 className="text-lg font-semibold mb-2">Price Details</h4>
+                            <div className="flex justify-between">
+                                <span>Base Fare</span>
+                                <span>$400</span>
+                            </div>
+                            <div className="flex justify-between">
+                                <span>Discount</span>
+                                <span>$400</span>
+                            </div>
+                            <div className="flex justify-between">
+                                <span>Taxes</span>
+                                <span>$400</span>
+                            </div>
+                            <div className="flex justify-between">
+                                <span>Service Fee</span>
+                                <span>$400</span>
+                            </div>
+                            <div className="flex justify-between font-semibold mt-2">
+                                <span>Total</span>
+                                <span>$400</span>
+                            </div>
                         </div>
                     </div>
                 </div>
+
             </div>
             <Footer />
         </div>
