3815fe67bdcf41865deb225fc39d4e1471b1dd0d
seifeddinesaad <saadseif1925@outlook.com>
2024-07-30 13:27:15 +0200

    adding edit pdp icon and customize ANTD tabs component

--- src/app/profile/components/Main.tsx
+++ src/app/profile/components/Main.tsx
@@ -1,19 +1,26 @@
 import React from 'react'
 import coverImage from '../../../../public/profile/coverImage.png'
+import editIcon from '../../../../public/profile/editIcon.png'
+
 import Image from 'next/image'
 import ProfileTabs from './ProfileTabs'
+
 const Main = () => {
     return (
         <div className='flex flex-col justify-center items-center'>
             <div className='flex flex-col justify-center items-center'>
                 <Image src={coverImage} alt="img" />
                 <div className='flex flex-col justify-center items-center gap-2'>
-                    <img src="https://i.ibb.co/vL7MPSV/113129314.jpg" alt="avatar" width="150" height={150} style={{
-                        borderRadius: "50%",
-                        marginTop: "-4rem",
-                        objectFit: "cover",
-                        border: "4px solid #ff8682"
-                    }} />
+                    <div className='flex flex-col justify-center items-center'>
+                        <img src="https://i.ibb.co/vL7MPSV/113129314.jpg" alt="avatar" width="150" height={150} style={{
+                            borderRadius: "50%",
+                            marginTop: "-4rem",
+                            objectFit: "cover",
+                            border: "4px solid #ff8682"
+                        }} />
+                        <Image src={editIcon} alt="icon" className='self-end mt-[-3rem] cursor-pointer'/>
+                    </div>
+
                     <h2 className='text-lg font-bold'>Saif Saad</h2>
                     <p className='text-gray-500'>saadseif304@gmail.com</p>
                 </div>
--- src/app/profile/components/ProfileTabs.tsx
+++ src/app/profile/components/ProfileTabs.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Tabs } from 'antd';
+import { ConfigProvider, Tabs } from 'antd';
 import type { TabsProps } from 'antd';
 
 const onChange = (key: string) => {
@@ -9,32 +9,43 @@ const onChange = (key: string) => {
 const items: TabsProps['items'] = [
   {
     key: '1',
-    label: 'Tab 1',
+    label: 'Account',
     children: 'Content of Tab Pane 1',
   },
   {
     key: '2',
-    label: 'Tab 2',
+    label: 'History',
     children: 'Content of Tab Pane 2',
   },
   {
     key: '3',
-    label: 'Tab 3',
+    label: 'Payment methods',
     children: 'Content of Tab Pane 3',
   },
 ];
 
 const ProfileTabs: React.FC = () => {
-  return <Tabs
-    defaultActiveKey="1"
-    items={items}
-    onChange={onChange}
-    centered
-    size='large'
-    style={{
-      width: "90%",
+  return <ConfigProvider
+    theme={{
+      token: {
+        colorPrimary: '#8dd3bb',
+        borderRadius: 2,
+        colorBgContainer: '#8dd3bb',
+      },
     }}
-  />;
+  >
+    <Tabs
+      defaultActiveKey="1"
+      items={items}
+      onChange={onChange}
+      centered
+      size='large'
+      style={{
+        width: "90%",
+      }}
+
+    />
+  </ConfigProvider>
 }
 
 export default ProfileTabs;
